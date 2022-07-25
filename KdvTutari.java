5bac26c97dc038249ea25cdb81e940350ddeb517
EMRE <yunuspker@gmail.com>
2022-07-25 21:37:15 +0300

    java101

---
+++ KdvTutari.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+public class KdvTutari {
+    
+    /**
+     * @param args
+     */
+    public static void main(String[] args ) {
+        
+        
+        Scanner input = new Scanner(System.in);
+        int tutar;
+        double kdvOrani = 0.18;
+        double kdvOrani1 = 0.18;
+        double kdvFiyat;
+        double kdvTutari;
+
+        System.out.print("Tutar giriniz : ");
+        tutar = input.nextInt();
+        
+        if(tutar <= 1000) {
+
+            kdvFiyat = (tutar * kdvOrani) + tutar ;
+            kdvTutari = kdvFiyat - tutar;
+            System.out.println("KDV'li Fiyat =  " + kdvFiyat);
+            System.out.println("KDV Fiyatı = " + kdvTutari);
+        }
+          else {
+
+                kdvFiyat = (tutar * kdvOrani1) + tutar ;
+                System.out.println("KDV'li Fiyat =" + kdvFiyat);
+                kdvTutari = kdvFiyat - tutar;
+                System.out.println("KDV Fiyatı = " + kdvTutari);
+            }
+        
+    }
+}
+    
+    
