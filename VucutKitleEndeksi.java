618e3e0616e2821113241d34d1d1b33062124b3e
EMRE <yunuspker@gmail.com>
2022-08-22 16:12:58 +0300

    Java101

---
+++ VucutKitleEndeksi.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class VücutKitleEndeksi {
+    
+
+public static void main(String[] args) {
+
+    double boy;
+    double kilo;
+    double vucutIndex;
+    Scanner input = new Scanner(System.in);
+
+    System.out.print("Boy Giriniz(Metre Cinsinden) = ");
+    boy = input.nextDouble();
+    System.out.print("Kilo Giriniz = ");
+    kilo = input.nextDouble();
+
+    vucutIndex = (kilo / boy) ;
+    System.out.println("Vücut Kitle Endeksi = " + vucutIndex);
+
+
+}
+
+    
+}
