1d6a73861ee28b4f76412ebd9959746a1987c365
EMRE <yunuspker@gmail.com>
2022-08-29 16:58:11 +0300

    Java101

---
+++ KuvvetleriBulma.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class KuvvetleriBulma {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n,a=0;
+        System.out.print("Bir Sayı Giriniz = ");
+        n = input.nextInt();
+
+        for(int i=1; i<n; i=i*4){
+            System.out.println("Dördün Kuvvetleri = " + i);
+            }
+            for(int i=1; i<n; i*=5){
+                System.out.println("Beşin Kuvvetleri = " + i);
+            }
+
+        }
+        
+    }
