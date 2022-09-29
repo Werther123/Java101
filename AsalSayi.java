2cc6d0623f908bddd51cd84dff88aa994d9a0aef
EMRE <yunuspker@gmail.com>
2022-09-29 10:57:36 +0300

    Java101

--- AsalSayi.java
+++ AsalSayi.java
@@ -4,9 +4,11 @@ public class AsalSayi {
 		for(int i = 1; i < 100; i++) {
 			counter = 0;
 			for(int j = 1; j < i; j++) {
-				if(i % j == 0)counter++;
+				if(i % j == 0)
+                counter++;
 			}
-			if(counter == 1) System.out.print(i+" ");
+			if(counter == 1) 
+            System.out.print(i+" ");
 		}
 	}
 }
