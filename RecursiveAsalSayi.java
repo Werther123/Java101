2cc19707488eb3114acc2a99a9e7453ff2f88eab
EMRE <yunuspker@gmail.com>
2022-10-14 10:06:25 +0300

    Java101

---
+++ RecursiveAsalSayi.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class RecursiveAsalSayi {
+
+    static boolean asal(int n, int j){
+
+        if(n == j)
+            return true;
+
+        if(n % j == 0)
+            return false;
+
+        return asal(n, j + 1);
+    }
+
+
+    public static void main(String[] args){
+        int x;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Sayıyı giriniz : ");
+        x = input.nextInt();
+        System.out.print(asal(x,2) ? x + " sayısı ASALDIR !" : x + " sayısı ASAL değildir !" );
+
+
+    }
+}
